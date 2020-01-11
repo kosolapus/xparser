de88972aafcc977edfe3fd76a442246b92709624
Mike Home <kosolapus@gmail.com>
2020-01-12 03:57:07 +0400

    Update Amital endpoint

--- app/Http/Controllers/Shop/AmitalController.php
+++ app/Http/Controllers/Shop/AmitalController.php
@@ -2,14 +2,10 @@
 
 namespace App\Http\Controllers\Shop;
 
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
-use App\Http\Controllers\Shop\DefaultController;
-
 class AmitalController extends DefaultController
 {
     //Работает не совсем корректно, не учитывает отсутствующие ISBN
-    protected $xpath = "//div[contains(@class,'price-list')]/table/tr/td[3]/strong";
+    protected $xpath = "//div[contains(@class,'final-price')]";
     protected $link_prefix = "https://amital.ru/product/search.html?wildcard=true&search=";
     protected $link_postfix = "";
 }
