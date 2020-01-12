32d57304ad8410280abd28baa18325a84a400e39
Mike Home <kosolapus@gmail.com>
2020-01-12 04:46:06 +0400

    Магазин не работает сейчас

--- app/Http/Controllers/Shop/ProsveshenieController.php
+++ app/Http/Controllers/Shop/ProsveshenieController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers\Shop;
 
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
-use App\Http\Controllers\Shop\DefaultController;
 
 class ProsveshenieController extends DefaultController
 {
   protected $xpath = "//span[contains(@class,'actual-price')]";
   protected $link_prefix = "https://shop.prosv.ru/search?q=";
   protected $link_postfix = "";
+
+  public function parseISBN()
+  {
+      $this->json[] = 0;
+  }
 }
